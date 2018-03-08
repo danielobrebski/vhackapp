b91f13476c326c07e480cbf35070183c9a766766
Daniel Obrębski <dobrebski@e-point.pl>
2018-03-08 17:10:06 +0100

    Backend starter

---
+++ backend/src/main/java/pl/pw/vhacks/VhacksApplication.java
@@ -0,0 +1,12 @@
+package pl.pw.vhacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhacksApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/pl/pw/vhacks/VhacksApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.pw.vhacks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VhacksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
