8566fda71419990314112520e84770cc7f8cb10f
Michal <m.star@wp.pl>
2018-03-09 14:00:07 +0100

    search component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HelloPageComponent} from './hello-page/hello-page.component';
+import {SearchComponent} from './search/search.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/hello', pathMatch: 'full'},
-  {path: 'hello', component: HelloPageComponent}]
+  {path: 'hello', component: HelloPageComponent},
+  {path: 'search', component: SearchComponent}
+  ]
 ;
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,19 +5,22 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HelloPageComponent} from './hello-page/hello-page.component';
 import {AppRoutingModule} from './/app-routing.module';
-import {MatToolbarModule, MatButtonModule} from '@angular/material';
+import {MatToolbarModule, MatButtonModule, MatInputModule} from '@angular/material';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloPageComponent
+    HelloPageComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
